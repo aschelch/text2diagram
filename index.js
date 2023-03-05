2f0914731c616dc113a4335adf77239c1307d62d
Aurélien Schelcher <a.schelcher@ubitransport.com>
2023-03-05 18:34:21 +0100

    feat(stats); add logs and stats

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser'
 import {Configuration, OpenAIApi} from 'openai'
 
+import Debug from 'debug'
+const debug = Debug('express:app')
+
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -24,14 +27,17 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', async function (req, res) {
+  debug('display homepage')
   res.render('index');
 });
 
 app.post('/generate', async function (req, res) {
 
   if(limiter.consumeSync('global') !== true){
+    debug('reached limit')
     return res.sendStatus(429);
   }
+
 
   const messages = [
     {role: 'system', content: 'You are a diagram generator using mermaid language. You will only respond with a mermaid script. Don\' give any explanation. Respond as short as possible'},
@@ -50,6 +56,8 @@ app.post('/generate', async function (req, res) {
   let result = completion.data.choices[0].message?.content
   .replace('```mermaid\n', '')
   .replace('```', '');
+
+  debug('diagram generated')
   
   res.json(result);
 });
